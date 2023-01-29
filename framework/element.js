515139ffcd0ef215031b86f9544f088f2a96a5f8
Eteims <8teims@gmail.com>
2023-01-30 00:41:49 +0100

    Added virtual capability

--- framework/element.js
+++ framework/element.js
@@ -1,10 +1,33 @@
-const createElement = tagName => (strings, ...args) => ({
-  type: tagName,
-  template: strings.reduce(
-    (acc, currentString, index) => acc + currentString + (args[index] || ""), ""
-  )
-});
+import { h } from "snabbdom/h";
 
+// initial template value
+const initialState = {
+  template: "",
+  on: {}
+}
+
+// reducer function
+const createReducer = args => (acc, currentString, index) => {
+  const currentArg = args[index];
+  
+  if (currentArg && currentArg.type === "event"){
+    return { ...acc, on: { click: currentArg.click } };
+  }
+  
+  return {
+  ...acc,
+  template: acc.template + currentString + (args[index] || "")
+  }
+};
+
+const createElement = tagName => ( strings, ...args) => {
+  const { template, on } = strings.reduce(createReducer(args), initialState);
+
+  return {
+    type: "element",
+    template: h(tagName, { on } , template)
+  };
+};
 
 export const div = createElement("div");
 export const p = createElement("p");
