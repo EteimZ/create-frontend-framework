dd623b1afdd2e17448fa7183749683a0fce920a8
Eteims <8teims@gmail.com>
2023-02-07 06:20:19 +0100

    Finished tutorial

--- src/user.js
+++ src/user.js
@@ -1,6 +1,19 @@
+import { createComponent } from "../framework";
 import { div } from "../framework/element";
 import { onClick } from "../framework/event";
 
-export const User = ({ firstName, lastName}) =>
-  div`${onClick(() => alert(firstName))} Hello ${firstName} ${lastName}`;
+// initial stagte of the component
+const initialState = { firstName: "Eteimorde", lastName: "Youdiowei" };
 
+// state mutating actions
+const methods = {
+  changeName: (state, firstName) => ({ ...state, firstName })
+};
+
+// component template
+const template = ({ firstName, lastName, methods }) =>
+  div`${onClick(() =>
+    methods.changeName("Jacob")
+  )} Hello ${firstName} ${lastName}`;
+
+export const User = createComponent({ template, methods, initialState });
